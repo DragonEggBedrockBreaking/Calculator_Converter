1946c8b28727a74919516ab6ac8d4fc02cd6e7b0
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-10-16 21:30:01 +0100

    Remove unnecessary code, only add graph checkbox if chosen option is an equation

--- src/main.cpp
+++ src/main.cpp
@@ -108,10 +108,13 @@ int main()
         // Maths dropdown
         if (maths_main) {
             ImGui::Begin("Maths dropdown", &maths_main);
-            ImGui::Checkbox("Graphs", &graphs);
             static int selected_maths_opt;
             static const char* maths_options[] {"Basic Operations", "Linear Equations", "Non Linear Equations", "Calculus", "Probability"};
             ImGui::Combo("Mathematical Operations", &selected_maths_opt, maths_options, IM_ARRAYSIZE(maths_options));
+
+            if (selected_maths_opt == 1 || selected_maths_opt == 2) {
+                ImGui::Checkbox("Graph", &graphs);
+            }
 
             switch (selected_maths_opt) {
             case 0:
@@ -129,15 +132,12 @@ int main()
                     ImGui::InputDouble("x +##foo1", &xcoef1_e1, 0.0, 0.0, "%.8f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(74);
                     ImGui::InputDouble("=##foo2", &num1_e1, 0.0, 0.0, "%.8f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(74);
                     ImGui::InputDouble("x +##foo3", &xcoef2_e1, 0.0, 0.0, "%.8f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(74);
                     ImGui::InputDouble("##foo4", &num2_e1, 0.0, 0.0, "%.8f");
 
                     if (ImGui::Button("Solve")) {
@@ -153,46 +153,35 @@ int main()
                     ImGui::InputDouble("x +##foo1", &xcoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo2", &ycoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo3", &num1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo4", &xcoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo5", &ycoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo6", &num2_e1, 0.0, 0.0, "%.5f");
 
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo7", &xcoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo8", &ycoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo9", &num1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo10", &xcoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo11", &ycoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo12", &num2_e2, 0.0, 0.0, "%.5f");
 
                     if (ImGui::Button("Solve")) {
@@ -210,93 +199,71 @@ int main()
                     ImGui::InputDouble("x +##foo1", &xcoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo2", &ycoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo3", &zcoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo4", &num1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo5", &xcoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo6", &ycoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo7", &zcoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo8", &num2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo9", &xcoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo10", &ycoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo11", &zcoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo12", &num1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo13", &xcoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo14", &ycoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo15", &zcoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo16", &num2_e2, 0.0, 0.0, "%.5f");
 
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo17", &xcoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo18", &ycoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo19", &zcoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo20", &num1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo21", &xcoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo22", &ycoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo23", &zcoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo24", &num2_e3, 0.0, 0.0, "%.5f");
 
                     if (ImGui::Button("Solve")) {
@@ -318,156 +285,117 @@ int main()
                     ImGui::InputDouble("x +##foo1", &xcoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo2", &ycoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo3", &zcoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo4", &acoef1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo5", &num1_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo6", &xcoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo7", &ycoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo8", &zcoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo9", &acoef2_e1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo10", &num2_e1, 0.0, 0.0, "%.5f");
 
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo11", &xcoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo12", &ycoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo13", &zcoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo14", &acoef1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo15", &num1_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo16", &xcoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo17", &ycoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo18", &zcoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo19", &acoef2_e2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo20", &num2_e2, 0.0, 0.0, "%.5f");
 
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo21", &xcoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo22", &ycoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo23", &zcoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo24", &acoef1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo25", &num1_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo26", &xcoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo27", &ycoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo28", &zcoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo29", &acoef2_e3, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo30", &num2_e3, 0.0, 0.0, "%.5f");
 
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo31", &xcoef1_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo32", &ycoef1_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo33", &zcoef1_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo34", &acoef1_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo35", &num1_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo36", &xcoef2_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("y +##foo37", &ycoef2_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("z +##foo38", &zcoef2_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("a +##foo39", &acoef2_e4, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo40", &num2_e4, 0.0, 0.0, "%.5f");
 
                     if (ImGui::Button("Solve")) {
@@ -497,23 +425,18 @@ int main()
                     ImGui::InputDouble("x^2 +##foo1", &x2coef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo2", &xcoef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo3", &num1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^2 +##foo4", &x2coef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo5", &xcoef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo6", &num2, 0.0, 0.0, "%.5f");
 
                     if (ImGui::Button("Solve")) {
@@ -533,34 +456,28 @@ int main()
                     ImGui::InputDouble("x^3 +##foo1", &x3coef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^2 +##foo2", &x2coef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo3", &xcoef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo4", &num1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^3 +##foo5", &x3coef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^2##foo6", &x2coef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo7", &xcoef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo8", &num2, 0.0, 0.0, "%.5f");
 
                     ImGui::Text("This solution uses the Newton-Raphson iterative method. Please enter the error leniency.");
+                    ImGui::PushItemWidth(610);
                     ImGui::InputDouble("##foo9", &error);
 
                     if (ImGui::Button("Solve")) {
@@ -577,42 +494,34 @@ int main()
                     ImGui::InputDouble("x^4 +##foo1", &x4coef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^3 +##foo2", &x3coef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^2 +##foo3", &x2coef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo4", &xcoef1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("=##foo5", &num1, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^4 +##foo6", &x4coef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^3 +##foo7", &x3coef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x^2##foo8", &x2coef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("x +##foo9", &xcoef2, 0.0, 0.0, "%.5f");
 
                     ImGui::SameLine();
-                    ImGui::PushItemWidth(55);
                     ImGui::InputDouble("##foo10", &num2, 0.0, 0.0, "%.5f");
 
                     ImGui::Text("This solution uses the Newton-Raphson iterative method. Please enter the error leniency.");
+                    ImGui::PushItemWidth(610);
                     ImGui::InputDouble("##foo11", &error);
 
                     if (ImGui::Button("Solve")) {
